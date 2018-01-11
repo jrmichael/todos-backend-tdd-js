6354d2bc62cafe554cca2594067c01a3c42deed9
Michal Bareja <michael.bareja@gmail.com>
2018-01-11 21:15:14 +0100

    sample koa app

---
+++ test/sample.spec.js
@@ -0,0 +1,25 @@
+const app = require('../app');
+
+let request;
+let server;
+
+beforeAll(() => {
+    server = app.listen(3001);
+    request = require('supertest')(server);
+});
+
+afterAll(() => {
+    server.close();
+});
+
+describe('server', () => {
+
+    it('responds to sample response', function () {
+        return request.get('/json')
+            .expect(200)
+            .then(response => {
+                expect(response.body.title).toBe('koa2 json');
+            });
+    });
+
+});
